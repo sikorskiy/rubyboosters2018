0891cc35a9eb57b5aa00cc76aceff65656b6a884
Sikorskiy <vasiliy.sikorskiy@gmail.com>
2018-11-01 13:21:35 +0300

    added heredoc.rb and hall_of_fame example

---
+++ 04.lesson/hall_of_fame_request.rb
@@ -0,0 +1,32 @@
+# you need to install gem 'httparty' before. Enter 'gem install httparty' in Linux console for installation
+require 'httparty' #library which makes creating request easier
+site = 'http://rubyboosters.herokuapp.com/halloffame'
+#hash-parameters for post request
+post_options = {
+  body: 
+  {
+    user: 'Vasiliy Sikorskiy',
+    score: 25
+  }
+}
+
+#hash-parameters for get request
+get_options = {
+  body:
+  {
+    user: 'Vasiliy Sikorskiy'
+  }
+}
+#this string sends user name and score to database and stores response in variable
+response = HTTParty.post(site, post_options)
+puts JSON.parse(response.body)
+#you can change some values:
+post_options[:body][:score] = 2
+#one more data sending
+response = HTTParty.post(site, post_options)
+puts JSON.parse(response.body)
+#here we get current Hall of fame for some user
+response = HTTParty.get(site, get_options)
+arr = JSON.parse(response.body).to_a
+puts arr[0]
+puts "User: #{arr[0]['user']}\nScore: #{arr[0]['score']}"
---
+++ 04.lesson/test_heredoc.rb
@@ -0,0 +1,8 @@
+d = 1
+s = <<~HEREDOC
+  Hello
+  What's HEREDOC your name?
+    And what so?
+  Here is number #{d}
+HEREDOC
+puts s
